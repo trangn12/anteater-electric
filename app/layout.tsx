e05fa4793341fd9be8975f22be6cdc29714baa5f
trangn12 <138056083+trangn12@users.noreply.github.com>
2025-08-01 00:32:43 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Anteater Electric Racing',
+  description: 'Official website for UCI’s Formula SAE Electric team',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
